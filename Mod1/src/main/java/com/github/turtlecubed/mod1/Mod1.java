bbaa614201656bad5c0b40004c91fa40cfbe1f50
Tristan Yoo <tristan.yoo@berkeley.edu>
2020-07-10 17:40:53 -0700

    logger instantiation message

--- Mod1/src/main/java/com/github/turtlecubed/mod1/Mod1.java
+++ Mod1/src/main/java/com/github/turtlecubed/mod1/Mod1.java
@@ -1,9 +1,16 @@
 package com.github.turtlecubed.mod1;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Mod1.MODID)
 public final class Mod1 {
     public static final String MODID = "mod1";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Mod1() {
+        LOGGER.debug("Hello from Mod1!");
+    }
 }
 
