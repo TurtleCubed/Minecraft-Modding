be6781b5a08e94b566d1b6758d669aff36f4fe60
Tristan Yoo <tristan.yoo@berkeley.edu>
2020-07-10 18:50:05 -0700

    added model and texture for orange_ingot

---
+++ Mod1/src/main/java/com/github/turtlecubed/mod1/ModEventSubscriber.java
@@ -0,0 +1,31 @@
+package com.github.turtlecubed.mod1;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@EventBusSubscriber(modid = Mod1.MODID, bus = EventBusSubscriber.Bus.MOD)
+public final class ModEventSubscriber {
+    @SubscribeEvent
+    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+        Item newIngot = setup(new Item(new Item.Properties()), "orange_ingot");
+        event.getRegistry().registerAll(newIngot);
+        //event.getRegistry().registerAll(
+        //        setup(new Item(new Item.Properties()), "orange_ingot")
+        //);
+
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+        return setup(entry, new ResourceLocation(Mod1.MODID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+        entry.setRegistryName(registryName);
+        return entry;
+    }
+
+}
